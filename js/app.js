dd25d62e0989dd3f34efe1339c4496bc7dee67a5
Szymon.Zacharewicz <szymon.zacharewicz@gmail.com>
2019-11-13 22:30:38 +0100

    changed way of grid generation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 let gameBlock = document.querySelectorAll(".game_block");
 let gameContainer = document.querySelector(".game_grid");
-let griddSize = 25;
+let gridArray = newLevel(25);
+createInOut(gridArray);
+createPass(gridArray);
+createGrid(gridArray);
+/*let griddSize = 25;
 console.log("start")
 for (let y=1;y<=griddSize;y++){
     for (let x=1;x<=griddSize;x++){
@@ -15,9 +19,9 @@ console.log("stop")
 let charPos = document.querySelector(".posX-1");
 let char = document.createElement("div");
 char.classList.add("char");
-charPos.appendChild(char);
+charPos.appendChild(char);*/
 
-document.addEventListener("keydown", function (elem) {
+/*document.addEventListener("keydown", function (elem) {
     if (elem.key==="ArrowRight"){
         let posX = char.parentElement.classList.value
         console.log(posX)
@@ -32,49 +36,59 @@ document.addEventListener("keydown", function (elem) {
     if (elem.key==="ArrowDown"){
         console.log("w dół")
     }
-})
-
+})*/
 
 function newLevel(gridSize){
-    console.log("start")
     let level =[];
     for (let y=1;y<=gridSize;y++){
         let levelY=[];
         for (let x=1;x<=gridSize;x++){
-            levelY.push({top: true, right: true, bottom: true, left:true})
-            let newBlock = document.createElement("div");
-            newBlock.classList.add("game_block")
-            newBlock.classList.add(`posX-${x}`);
-            newBlock.classList.add(`posY-${y}`);
-            gameContainer.appendChild(newBlock)
+            levelY.push({wall: true,
+                         isEnter: false,
+                         isExit: false});
         }
         level.push(levelY)
     }
-    console.log("stop");
     return level;
 }
 
-console.log(newLevel(25))
-
-function combinations(levels){
-    console.log("start")
-    let output=[];
-    for (let i=0;i<=levels;i++){
-        let output1=[];
-        for (let j=0;j<=levels;j++){
-            let output2=[];
-            for (let k=0;k<=levels;k++){
-                let output3=[];
-                for (let l=0;l<=levels;l++){
-                    output3.push(l);
-                }
-                output2.push(output3)
-            }
-            output1.push(output2)
-        }
-        output.push(output1)
-    }
-    console.log("stop")
-    return output;
+function createInOut(array) {
+    let ranInY = Math.round(Math.random() * (array.length -1));
+    let ranInX= Math.round(Math.random() * (array.length -1));
+    let ranOutY= Math.round(Math.random() * (array.length -1));
+    let ranOutX= Math.round(Math.random() * (array.length -1));
+    array[ranInY][ranInX].isEnter=true;
+    array[ranInY][ranInX].wall=false;
+    array[ranOutY][ranOutX].isExit=true;
+    array[ranOutY][ranOutX].wall=false;
+    /*need to add condition when enter is same as exit*/
+    console.log(array[ranInY][ranInX], array[ranOutY][ranOutX]);
 }
-console.log(combinations(1));
+
+function createPass(array, callback) {
+
+}
+
+function createGrid(array) {
+    array.forEach(function (elem, indexy) {
+        elem.forEach(function (object, indexx) {
+            let newBlock = document.createElement("div");
+            newBlock.classList.add("game_block");
+            newBlock.classList.add(`posX-${indexx}`);
+            newBlock.classList.add(`posY-${indexy}`);
+            if (object.isEnter===true){
+                newBlock.classList.add("enter");
+            }else if(object.isExit===true){
+                newBlock.classList.add("exit");
+            }else if(object.wall===false){
+                newBlock.classList.add("pass");
+            }else if(object.wall===true){
+                newBlock.classList.add("wall");
+            }else{
+                console.log("coś poszło nie tak podczas generacji poziomu");
+            }
+            gameContainer.appendChild(newBlock);
+        })
+    })
+}
+
