735221a9eefba4f0184748a64f1d44b766c34b3f
Szymon.Zacharewicz <szymon.zacharewicz@gmail.com>
2019-11-26 14:15:53 +0100

    reworking REACT

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import Hello from "../Hello/Hello";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Hello/>
+       <p>TEST</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hello/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hello() {
+    return (
+
+                <p>Hello</p>
+
+    );
+}
+
+export default Hello;
