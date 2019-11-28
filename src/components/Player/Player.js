8d3d7c4c1703c893409447fc200b1183d9937e4b
Szymon.Zacharewicz <szymon.zacharewicz@gmail.com>
2019-11-29 00:12:15 +0100

    animations

---
+++ src/components/Player/Player.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import './Player.scss'
+
+class Player extends Component{
+    render() {
+        return(
+            <div className="gracz">
+
+            </div>
+        )
+    }
+}
+
+export default Player;
