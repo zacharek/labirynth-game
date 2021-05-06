5dd56c633c4285c99622210fa926ee44ecf479aa
Szymon.Zacharewicz <szymon.zacharewicz@gmail.com>
2021-05-06 19:45:05 +0200

    move generateLevel to proper location

--- src/components/GameContainer/GameContainer.js
+++ src/components/GameContainer/GameContainer.js
@@ -1,43 +1,43 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import './GameContainer.scss'
 import GameGrid from "../GameGrid/GameGrid";
-import generateLevel from "../LevelGeneration/generateLevel";
+import generateLevel from "./generateLevel";
 
-class GameContainer extends Component{
-    state={
-        singleBlockSize:window.innerHeight * 0.88 / this.props.genSettings.entrySize,
-        level:[],
-        size:this.props.genSettings.entrySize,
-        difficulty:this.props.genSettings.entryDifficulty,
-        player1positionAbsolute:[0,0],
-        player1positionRelative:[0,0],
-        player1moving:"standby",
-        player1VelocityX:0,
-        player1VelocityY:0
+class GameContainer extends Component {
+    state = {
+        singleBlockSize: window.innerHeight * 0.88 / this.props.genSettings.entrySize,
+        level: [],
+        size: this.props.genSettings.entrySize,
+        difficulty: this.props.genSettings.entryDifficulty,
+        player1positionAbsolute: [0, 0],
+        player1positionRelative: [0, 0],
+        player1moving: "standby",
+        player1VelocityX: 0,
+        player1VelocityY: 0
     };
     shouldComponentUpdate(prevProps, prevState) {
         let lvl;
         let singleBlockSize = this.state.singleBlockSize;
-        
+
         if (this.props.start !== prevProps.start) {
             lvl = generateLevel(this.state.size, this.state.difficulty);
-        }else if (this.state.difficulty !== prevState.difficulty && this.state.size !== prevState.size) {
+        } else if (this.state.difficulty !== prevState.difficulty && this.state.size !== prevState.size) {
             lvl = generateLevel(this.state.size + 5, 3);
             singleBlockSize = window.innerHeight * 0.88 / (this.state.size + 5)
-        }else if (this.state.size !== prevState.size) {
+        } else if (this.state.size !== prevState.size) {
             lvl = generateLevel(this.state.size + 5, this.state.difficulty);
             singleBlockSize = window.innerHeight * 0.88 / (this.state.size + 5)
-        }else if (this.state.difficulty !== prevState.difficulty) {
+        } else if (this.state.difficulty !== prevState.difficulty) {
             lvl = generateLevel(this.state.size, this.state.difficulty + 1)
-        }else if (this.state.player1positionRelative !== prevState.player1positionRelative) {
+        } else if (this.state.player1positionRelative !== prevState.player1positionRelative) {
             return true
-        }else if(this.state.singleBlockSize !== prevState.singleBlockSize){
-            let scaleMultiplier = window.innerHeight/1512;
+        } else if (this.state.singleBlockSize !== prevState.singleBlockSize) {
+            let scaleMultiplier = window.innerHeight / 1512;
             this.setState({
-                player1positionRelative:[this.state.player1positionRelative[0]*scaleMultiplier,this.state.player1positionRelative[1]*scaleMultiplier]
+                player1positionRelative: [this.state.player1positionRelative[0] * scaleMultiplier, this.state.player1positionRelative[1] * scaleMultiplier]
             })
             return true
-        }else {
+        } else {
             return this.state.level[0] !== prevState.level[0];
         }
         this.setState({
@@ -49,118 +49,118 @@ class GameContainer extends Component{
         });
         return true
     }
-    componentDidMount(){
-        window.addEventListener("keydown",(e)=>{
-            if(e.code === "KeyD" || e.code === "ArrowRight"){
+    componentDidMount() {
+        window.addEventListener("keydown", (e) => {
+            if (e.code === "KeyD" || e.code === "ArrowRight") {
                 this.handleMoveRight()
-            }else if(e.code === "KeyA" || e.code === "ArrowLeft"){
+            } else if (e.code === "KeyA" || e.code === "ArrowLeft") {
                 this.handleMoveLeft()
-            }else if(e.code === "KeyW" || e.code === "ArrowUp"){
+            } else if (e.code === "KeyW" || e.code === "ArrowUp") {
                 this.handleMoveUp()
-            }else if(e.code === "KeyS" || e.code === "ArrowDown") {
+            } else if (e.code === "KeyS" || e.code === "ArrowDown") {
                 this.handleMoveDown()
             }
         });
-        window.addEventListener("resize", ()=>{
+        window.addEventListener("resize", () => {
             let singleBlockSize;
-            if (this.state.size !== this.props.genSettings.entrySize){
-                singleBlockSize = window.innerHeight*0.88/this.state.size
-            }else{
-                singleBlockSize = window.innerHeight*0.88/this.props.genSettings.entrySize
+            if (this.state.size !== this.props.genSettings.entrySize) {
+                singleBlockSize = window.innerHeight * 0.88 / this.state.size
+            } else {
+                singleBlockSize = window.innerHeight * 0.88 / this.props.genSettings.entrySize
             }
             this.setState({
-                singleBlockSize:singleBlockSize
+                singleBlockSize: singleBlockSize
             })
         })
     }
-    handleMoveRight=()=>{
+    handleMoveRight = () => {
         let singleBlockSize = this.state.singleBlockSize;
         let positionAbsolute = this.state.player1positionAbsolute;
-        let positionRelative = [this.state.player1positionRelative[0]+(singleBlockSize/16),
-            this.state.player1positionRelative[1]];
-        if (positionRelative[0]+(singleBlockSize*0.375)>singleBlockSize && (positionRelative[1]<0 || positionRelative[1]+(singleBlockSize*0.125)>singleBlockSize)){
-            positionRelative=this.state.player1positionRelative
-        }else if(positionRelative[0]+(singleBlockSize*0.375)>singleBlockSize){
-            let offset = positionRelative[0]-singleBlockSize;
-            positionAbsolute=[positionAbsolute[0]+1,positionAbsolute[1]];
-            positionRelative=[offset,positionRelative[1]]
+        let positionRelative = [this.state.player1positionRelative[0] + (singleBlockSize / 16),
+        this.state.player1positionRelative[1]];
+        if (positionRelative[0] + (singleBlockSize * 0.375) > singleBlockSize && (positionRelative[1] < 0 || positionRelative[1] + (singleBlockSize * 0.125) > singleBlockSize)) {
+            positionRelative = this.state.player1positionRelative
+        } else if (positionRelative[0] + (singleBlockSize * 0.375) > singleBlockSize) {
+            let offset = positionRelative[0] - singleBlockSize;
+            positionAbsolute = [positionAbsolute[0] + 1, positionAbsolute[1]];
+            positionRelative = [offset, positionRelative[1]]
         }
-        if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].isExit===1){
+        if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].isExit === 1) {
             this.handleCompleted()
-        }else if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].wall===0){
-            this.setState({player1positionAbsolute: positionAbsolute, player1positionRelative: positionRelative, player1moving:"move-right"})
+        } else if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].wall === 0) {
+            this.setState({ player1positionAbsolute: positionAbsolute, player1positionRelative: positionRelative, player1moving: "move-right" })
         }
     };
-    handleMoveLeft=()=>{
+    handleMoveLeft = () => {
         let singleBlockSize = this.state.singleBlockSize;
         let positionAbsolute = this.state.player1positionAbsolute;
-        let positionRelative = [this.state.player1positionRelative[0]-(singleBlockSize/16),
-            this.state.player1positionRelative[1]];
-        if (positionRelative[0]<0 && (positionRelative[1]<0 || positionRelative[1]+(singleBlockSize*0.125)>singleBlockSize)){
-            positionRelative=this.state.player1positionRelative
-        }else if(positionRelative[0]<0){
-            let offset = singleBlockSize+positionRelative[0];
-            positionAbsolute=[positionAbsolute[0]-1,positionAbsolute[1]];
-            positionRelative=[offset,positionRelative[1]]
+        let positionRelative = [this.state.player1positionRelative[0] - (singleBlockSize / 16),
+        this.state.player1positionRelative[1]];
+        if (positionRelative[0] < 0 && (positionRelative[1] < 0 || positionRelative[1] + (singleBlockSize * 0.125) > singleBlockSize)) {
+            positionRelative = this.state.player1positionRelative
+        } else if (positionRelative[0] < 0) {
+            let offset = singleBlockSize + positionRelative[0];
+            positionAbsolute = [positionAbsolute[0] - 1, positionAbsolute[1]];
+            positionRelative = [offset, positionRelative[1]]
         }
-        if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].isExit===1){
+        if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].isExit === 1) {
             this.handleCompleted()
-        }else if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].wall===0){
-            this.setState({player1positionAbsolute: positionAbsolute, player1positionRelative: positionRelative, player1moving:"move-left"})
+        } else if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].wall === 0) {
+            this.setState({ player1positionAbsolute: positionAbsolute, player1positionRelative: positionRelative, player1moving: "move-left" })
         }
     };
-    handleMoveUp=()=>{
-        let singleBlockSize = this.state.singleBlockSize;
-        let positionAbsolute = this.state.player1positionAbsolute;
-        let positionRelative = [this.state.player1positionRelative[0],
-            this.state.player1positionRelative[1]-(singleBlockSize/16)];
-        if (positionRelative[1]<0 && (positionRelative[0]<0 || positionRelative[0]+(singleBlockSize*0.375)>singleBlockSize)){
-            positionRelative=this.state.player1positionRelative
-        }else if(positionRelative[1]<0){
-            let offset = singleBlockSize+positionRelative[1];
-            positionAbsolute=[positionAbsolute[0],positionAbsolute[1]-1];
-            positionRelative=[positionRelative[0],offset]
-        }
-        if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].isExit===1){
-            this.handleCompleted()
-        }else if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].wall===0){
-            this.setState({player1positionAbsolute: positionAbsolute, player1positionRelative: positionRelative, player1moving:"move-up"})
-        }
-    };
-    handleMoveDown=()=>{
+    handleMoveUp = () => {
         let singleBlockSize = this.state.singleBlockSize;
         let positionAbsolute = this.state.player1positionAbsolute;
         let positionRelative = [this.state.player1positionRelative[0],
-            this.state.player1positionRelative[1]+(singleBlockSize/16)];
-        if (positionRelative[1]+(singleBlockSize*0.125)>singleBlockSize && (positionRelative[0]<0 || positionRelative[0]+(singleBlockSize*0.375)>singleBlockSize)){
-            positionRelative=this.state.player1positionRelative
-        }else if(positionRelative[1]+(singleBlockSize*0.125)>singleBlockSize){
-            let offset = positionRelative[1]-singleBlockSize;
-            positionAbsolute=[positionAbsolute[0],positionAbsolute[1]+1];
-            positionRelative=[positionRelative[0],offset]
+        this.state.player1positionRelative[1] - (singleBlockSize / 16)];
+        if (positionRelative[1] < 0 && (positionRelative[0] < 0 || positionRelative[0] + (singleBlockSize * 0.375) > singleBlockSize)) {
+            positionRelative = this.state.player1positionRelative
+        } else if (positionRelative[1] < 0) {
+            let offset = singleBlockSize + positionRelative[1];
+            positionAbsolute = [positionAbsolute[0], positionAbsolute[1] - 1];
+            positionRelative = [positionRelative[0], offset]
         }
-        if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].isExit===1){
+        if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].isExit === 1) {
             this.handleCompleted()
-        }else if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].wall===0){
-            this.setState({player1positionAbsolute: positionAbsolute, player1positionRelative: positionRelative, player1moving:"move-down"})
+        } else if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].wall === 0) {
+            this.setState({ player1positionAbsolute: positionAbsolute, player1positionRelative: positionRelative, player1moving: "move-up" })
         }
     };
-    handleCompleted=()=>{
-        if (this.state.size>100) {
+    handleMoveDown = () => {
+        let singleBlockSize = this.state.singleBlockSize;
+        let positionAbsolute = this.state.player1positionAbsolute;
+        let positionRelative = [this.state.player1positionRelative[0],
+        this.state.player1positionRelative[1] + (singleBlockSize / 16)];
+        if (positionRelative[1] + (singleBlockSize * 0.125) > singleBlockSize && (positionRelative[0] < 0 || positionRelative[0] + (singleBlockSize * 0.375) > singleBlockSize)) {
+            positionRelative = this.state.player1positionRelative
+        } else if (positionRelative[1] + (singleBlockSize * 0.125) > singleBlockSize) {
+            let offset = positionRelative[1] - singleBlockSize;
+            positionAbsolute = [positionAbsolute[0], positionAbsolute[1] + 1];
+            positionRelative = [positionRelative[0], offset]
+        }
+        if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].isExit === 1) {
+            this.handleCompleted()
+        } else if (this.state.level[1][positionAbsolute[1]][positionAbsolute[0]].wall === 0) {
+            this.setState({ player1positionAbsolute: positionAbsolute, player1positionRelative: positionRelative, player1moving: "move-down" })
+        }
+    };
+    handleCompleted = () => {
+        if (this.state.size > 100) {
             //finished screen
-        }else if(this.state.size>40 && this.state.difficulty<9){
+        } else if (this.state.size > 40 && this.state.difficulty < 9) {
             this.props.score(this.state.size, this.state.difficulty);
-            this.setState({difficulty:this.state.difficulty+1})
-        }else if(this.state.size>40 && this.state.difficulty===9){
+            this.setState({ difficulty: this.state.difficulty + 1 })
+        } else if (this.state.size > 40 && this.state.difficulty === 9) {
             this.props.score(this.state.size, this.state.difficulty);
-            this.setState({difficulty:5, size:this.state.size+5})
-        }else if (this.state.difficulty<6){
+            this.setState({ difficulty: 5, size: this.state.size + 5 })
+        } else if (this.state.difficulty < 6) {
             this.props.score(this.state.size, this.state.difficulty);
-            this.setState({difficulty:this.state.difficulty+1})
-        }else if (this.state.difficulty===6){
+            this.setState({ difficulty: this.state.difficulty + 1 })
+        } else if (this.state.difficulty === 6) {
             this.props.score(this.state.size, this.state.difficulty);
-            this.setState({difficulty:3, size:this.state.size+5})
-        }else{
+            this.setState({ difficulty: 3, size: this.state.size + 5 })
+        } else {
             console.log("coś jest nie tak")
         }
     };
@@ -171,24 +171,24 @@ class GameContainer extends Component{
         this.position.y += this.velocity.y;
         this.velocity.x *= this.inertia;
         this.velocity.y *= this.inertia;*/
-        if(this.state.level.length!==0){
-            let posTop = (this.state.player1positionAbsolute[1] * this.state.singleBlockSize + this.state.player1positionRelative[1]) - this.state.singleBlockSize*this.state.size;
+        if (this.state.level.length !== 0) {
+            let posTop = (this.state.player1positionAbsolute[1] * this.state.singleBlockSize + this.state.player1positionRelative[1]) - this.state.singleBlockSize * this.state.size;
             let posLeft = this.state.player1positionAbsolute[0] * this.state.singleBlockSize + this.state.player1positionRelative[0];
-            let playerShadow = this.state.singleBlockSize/8;
-            let playerShadowWidth = playerShadow*3;
-            let resolutionMultiplier = 32*((window.innerHeight/1512)*(10/this.state.size)-1);
-            let offsetTop = -(playerShadow*3.25)+ resolutionMultiplier;
-            let offsetLeft = -(playerShadow*3/8)+ resolutionMultiplier;
-            return(
+            let playerShadow = this.state.singleBlockSize / 8;
+            let playerShadowWidth = playerShadow * 3;
+            let resolutionMultiplier = 32 * ((window.innerHeight / 1512) * (10 / this.state.size) - 1);
+            let offsetTop = -(playerShadow * 3.25) + resolutionMultiplier;
+            let offsetLeft = -(playerShadow * 3 / 8) + resolutionMultiplier;
+            return (
                 <div className="game__container">
-                    <GameGrid level={this.state.level[1]} size={this.state.singleBlockSize}/>
-                    <div className="player__shadow" style={{top:posTop+"px", left:posLeft+"px", height:playerShadow, width:playerShadowWidth}}>
-                        <div className={`character ${this.state.player1moving}`} style={{top:offsetTop, left:offsetLeft, transform:`scale(${10/this.state.size*(window.innerHeight/1512)})`}}/>
+                    <GameGrid level={this.state.level[1]} size={this.state.singleBlockSize} />
+                    <div className="player__shadow" style={{ top: posTop + "px", left: posLeft + "px", height: playerShadow, width: playerShadowWidth }}>
+                        <div className={`character ${this.state.player1moving}`} style={{ top: offsetTop, left: offsetLeft, transform: `scale(${10 / this.state.size * (window.innerHeight / 1512)})` }} />
                     </div>
                 </div>
             )
-        }else{
-            return(
+        } else {
+            return (
                 <div className="game__container">
                     <div className="message">
                         <p onClick={this.props.startOnClick}>START</p>
